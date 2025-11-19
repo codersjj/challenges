b9cef8c15afc905b6a4ac853c20a2348868b4890
codersjj <shanewestlife@outlook.com>
2025-11-19 18:46:16 +0800

    feat: initialize React Tic Tac Toe challenge

--- 02_React_Tic_Tac_Toe/react-tic-tac-toe-app/src/main.tsx
+++ 02_React_Tic_Tac_Toe/react-tic-tac-toe-app/src/main.tsx
@@ -1,10 +1,102 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/* eslint-disable react-refresh/only-export-components */
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rowStyle = {
+  display: 'flex'
+}
+
+const squareStyle = {
+  'width':'60px',
+  'height':'60px',
+  'backgroundColor': '#ddd',
+  'margin': '4px',
+  'display': 'flex',
+  'justifyContent': 'center',
+  'alignItems': 'center',
+  'fontSize': '20px',
+  'color': 'white'
+}
+
+const boardStyle: React.CSSProperties = {
+  'backgroundColor': '#eee',
+  'width': '208px',
+  'alignItems': 'center',
+  'justifyContent': 'center',
+  'display': 'flex',
+  'flexDirection': 'column',
+  'border': '3px #eee solid'
+}
+
+const containerStyle: React.CSSProperties = {
+  'display': 'flex',
+  'alignItems': 'center',
+  'flexDirection': 'column'
+}
+
+const instructionsStyle = {
+  'marginTop': '5px',
+  'marginBottom': '5px',
+  'fontWeight': 'bold',
+  'fontSize': '16px',
+}
+
+const buttonStyle = {
+  'marginTop': '15px',
+  'marginBottom': '16px',
+  'width': '80px',
+  'height': '40px',
+  'backgroundColor': '#8acaca',
+  'color': 'white',
+  'fontSize': '16px',
+}
+
+function Square() {
+  return (
+    <div
+      className="square"
+      style={squareStyle}>
+    </div>
+  );
+}
+
+function Board() {
+  return (
+    <div style={containerStyle} className="gameBoard">
+      <div id="statusArea" className="status" style={instructionsStyle}>Next player: <span>X</span></div>
+      <div id="winnerArea" className="winner" style={instructionsStyle}>Winner: <span>None</span></div>
+      <button style={buttonStyle}>Reset</button>
+      <div style={boardStyle}>
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<Game />);
